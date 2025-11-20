a61670684852a5bd0b92277782f1e1a045dbbce2
6611130091-a55y <6611130091@mut.ac.th>
2025-11-20 12:33:26 +0700

    feat: add reset button to counter app

--- lib/main.dart
+++ lib/main.dart
@@ -98,11 +98,26 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Row(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
+    FloatingActionButton(
+      onPressed: () {
+        setState(() {
+          _counter = 0;
+        });
+      },
+      tooltip: 'Reset',
+      child: const Icon(Icons.refresh),
+    ),
+    const SizedBox(width: 10),
+    FloatingActionButton(
+      onPressed: _incrementCounter,
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    ),
+  ],
+), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
